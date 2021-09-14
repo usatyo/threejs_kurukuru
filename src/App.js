3bee67a486a3163fdc357eac9c670289e455bb50
usatyo <yu0424.math8128@gmail.com>
2021-09-14 11:09:05 +0900

    threejs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const loader = new GLTFLoader();
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcce0ff);
+scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+// const light = new THREE.PointLight( 0xffffff, 1, 100 );
+// light.position.set( 2, 2, 2 );
+// scene.add( light );
+
+const light2 = new THREE.AmbientLight( 0x666666, 1 ); // soft white light
+scene.add( light2 );
+
+const light = new THREE.DirectionalLight(0xdfebff, 1);
+light.position.set(50, 200, 100);
+light.position.multiplyScalar(1.3);
+
+light.castShadow = true;
+
+light.shadow.mapSize.width = 1024;
+light.shadow.mapSize.height = 1024;
+
+const d = 300;
+
+light.shadow.camera.left = - d;
+light.shadow.camera.right = d;
+light.shadow.camera.top = d;
+light.shadow.camera.bottom = - d;
+
+light.shadow.camera.far = 1000;
+
+scene.add(light);
+
+const size = 10;
+const divisions = 10;
+
+const gridHelper = new THREE.GridHelper(size, divisions);
+scene.add(gridHelper);
+
+console.log(scene);
+
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+
+loader.load(
+  // resource URL
+  // '../public/cube.glb',
+  '/tree.gltf',
+  // called when the resource is loaded
+  function (gltf) {
+
+    console.log("aaa");
+
+    scene.add(gltf.scene);
+
+    console.log(gltf);
+
+    // gltf.animations; // Array<THREE.AnimationClip>
+    // gltf.scene; // THREE.Group
+    // gltf.scenes; // Array<THREE.Group>
+    // gltf.cameras; // Array<THREE.Camera>
+    // gltf.asset; // Object
+
+  },
+  // called while loading is progressing
+  function (xhr) {
+
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+  },
+  // called when loading has errors
+  function (error) {
+    console.error(error);
+    console.log('An error happened');
+
+  }
+);
+
+controls.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+animate();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      a
     </div>
   );
 }
